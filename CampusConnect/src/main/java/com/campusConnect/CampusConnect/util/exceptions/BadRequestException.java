534dbcc8d50b9490174392ce7a7134cd9919a6cb
Juan Camilo <jccaste1002@gmail.com>
2024-05-23 14:51:46 -0500

    TB-034/ Creacion BadRequestException

---
+++ CampusConnect/src/main/java/com/campusConnect/CampusConnect/util/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.campusConnect.CampusConnect.util.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
